2d0673a3dc3c4e86e9f5084cba87ae1c9d759684
Lakshman <lakshman.r.2008@gmail.com>
2025-07-30 09:56:02 -0400

    Merged all parts together, need to work on ai format and geopy for single coulmn addresses

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from Excel_interpreter import get_first_5_rows_as_dict, AI_generate_json_structu
 import json
 from row_parsing import gather_row_data
 from Excel_builder import export_to_excel
+from jsons_to_excel import append_cleaned_json_to_excel
 
 
 snipit = get_first_5_rows_as_dict("test.xlsx")
@@ -14,3 +15,5 @@ with open("info.json", "r") as f:
 
 results = gather_row_data('test.xlsx', info_json)
 export_to_excel(results, "output.xlsx")
+
+append_cleaned_json_to_excel(directory=".", output_excel="output.xlsx")
